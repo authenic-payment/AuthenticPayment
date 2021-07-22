12ac25cb23beba965595a4d266a045ecb11350eb
Mitchell - Timing <mitchell_bink@hotmail.com>
2021-07-22 10:04:39 +0200

    updated socials

--- AuthenticPayment/AuthenticPayment/Views/AuthenthicatedViews/WalletPage.xaml.cs
+++ AuthenticPayment/AuthenticPayment/Views/AuthenthicatedViews/WalletPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-
+using stellar_dotnet_sdk;
+using stellar_dotnet_sdk.responses;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +14,47 @@ namespace AuthenticPayment.Views.AuthenthicatedViews
         {
             InitializeComponent();
         }
+
+
+        //Set network and server
+        public void GenerateAccountKeypair()
+        {
+            //Generate a random KeyPair
+            KeyPair keypair = KeyPair.FromAccountId("");
+
+            //Show the KeyPair public address and secret seed
+            Console.WriteLine("Account ID: " + keypair.AccountId);
+            Console.WriteLine("Secret: " + keypair.SecretSeed);
+        }
+
+        //Set network and server
+        public async Task GetAccountBalance()
+        {
+            //Set network and server
+            Network network = new Network("Test SDF Network ; September 2015");
+            Server server = new Server("https://horizon-testnet.stellar.org");
+
+            //Generate a keypair from the account id.
+            KeyPair keypair = KeyPair.FromAccountId("ACCOUNT_ID");
+
+            //Load the account
+            AccountResponse accountResponse = await server.Accounts.Account(keypair.AccountId);
+
+            //Get the balance
+            Balance[] balances = accountResponse.Balances;
+
+            //Show the balance
+            for (int i = 0; i < balances.Length; i++)
+            {
+                Balance asset = balances[i];
+                Console.WriteLine("Asset Code: " + asset.AssetCode);
+                Console.WriteLine("Asset Amount: " + asset.BalanceString);
+            }
+        }
     }
 }
+
+
+
+
+
--- AuthenticPayment/AuthenticPayment/Views/Socials/SocialsPage.xaml.cs
+++ AuthenticPayment/AuthenticPayment/Views/Socials/SocialsPage.xaml.cs
@@ -26,6 +26,68 @@ namespace AuthenticPayment.Views.Socials
             };
             telegramGesture.Tapped += TelegramGesture_Tapped;
             TelegramLabel.GestureRecognizers.Add(telegramGesture);
+
+            TapGestureRecognizer FacebookGesture = new TapGestureRecognizer()
+            {
+                NumberOfTapsRequired = 1,
+            };
+            FacebookGesture.Tapped += FacebookGesture_Tapped; ;
+            FacebookLabel.GestureRecognizers.Add(FacebookGesture);
+
+            TapGestureRecognizer TwitterGesture = new TapGestureRecognizer()
+            {
+                NumberOfTapsRequired = 1,
+            };
+            TwitterGesture.Tapped += TwitterGesture_Tapped; ;
+            TwitterLabel.GestureRecognizers.Add(TwitterGesture);
+
+            TapGestureRecognizer WebsiteGesture = new TapGestureRecognizer()
+            {
+                NumberOfTapsRequired = 1,
+            };
+            WebsiteGesture.Tapped += WebsiteGesture_Tapped; ;
+            WebsiteLabel.GestureRecognizers.Add(WebsiteGesture);
+
+            TapGestureRecognizer EmailGesture = new TapGestureRecognizer()
+            {
+                NumberOfTapsRequired = 1,
+            };
+            EmailGesture.Tapped += EmailGesture_Tapped; ;
+            EmailLabel.GestureRecognizers.Add(EmailGesture);
+
+
+            TapGestureRecognizer RedditGesture = new TapGestureRecognizer()
+            {
+                NumberOfTapsRequired = 1,
+            };
+            RedditGesture.Tapped += RedditGesture_Tapped; ; ;
+            RedditLabel.GestureRecognizers.Add(RedditGesture);
+        }
+
+        private void RedditGesture_Tapped(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync("https://www.reddit.com/r/OfficialApay/");
+        }
+
+        private void EmailGesture_Tapped(object sender, EventArgs e)
+        {
+            var address = "support@authentic-payment.com";
+            Launcher.OpenAsync($"mailto:{address}");
+        }
+
+        private void WebsiteGesture_Tapped(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync("https://www.Authentic-Payment.com");
+        }
+
+        private void TwitterGesture_Tapped(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync("https://twitter.com/officialapay?s=11");
+        }
+
+        private void FacebookGesture_Tapped(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync("https://www.facebook.com/Authentic-Payment-103724298649650");
         }
 
         private void TelegramGesture_Tapped(object sender, EventArgs e)
@@ -33,9 +95,6 @@ namespace AuthenticPayment.Views.Socials
             Launcher.OpenAsync("https://t.me/Authenticpayment");
         }
 
-        private void Telegram_clicked(object sender, EventArgs e)
-        {
-            Launcher.OpenAsync("https://t.me/Authenticpayment");
-        }
+
     }
 }
